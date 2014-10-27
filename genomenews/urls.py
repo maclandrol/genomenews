39d255d9f6c116057b24f7e21a8d63ba3de1f8f4
Marc-André Legault <legaultmarc@gmail.com>
2014-10-26 22:40:09 -0400

    The template is now a pretty good clone.

--- genomenews/urls.py
+++ genomenews/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'news.views.home')
+    url(r'^$', 'news.views.home', name='home')
 )
 
